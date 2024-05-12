351c35c9eb21ec35c70afdc0ae7a99df7fb64bbc
FrankGelashvili421 <eldarigelashvili421@gmail.com>
2024-05-12 14:33:05 +0400

    commit branch

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+let product = document.querySelector('.product')
+let sliding = document.querySelector('.rightSlide')
+let contact = document.querySelector('.contact')
+let contactlist = document.querySelector('.contactList')
+
+product.addEventListener('mouseenter', ()=>{
+    sliding.style.height = sliding.scrollHeight + 'px';
+})
+product.addEventListener('mouseleave', ()=>{
+    sliding.style.height = '0px';
+})
+
+contact.addEventListener('mouseenter', ()=>{
+    contactlist.style.height = contactlist.scrollHeight + 'px';
+})
+
+contact.addEventListener('mouseleave', ()=>{
+    contactlist.style.height = '0px';
+});

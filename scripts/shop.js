26b37941ea68f7fb20d503d738a5ab8c3742e491
FrankGelashvili421 <eldarigelashvili421@gmail.com>
2024-05-12 16:23:29 +0400

    masive added

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,6 +1,7 @@
 let product = document.querySelector('.product')
 let sliding = document.querySelector('.Slide')
-
+let contact = document.querySelector('.contact')
+let contactlist = document.querySelector('.contactList')
 
 product.addEventListener('mouseenter', ()=>{
     sliding.style.height = sliding.scrollHeight + 'px';
@@ -8,3 +9,11 @@ product.addEventListener('mouseenter', ()=>{
 product.addEventListener('mouseleave', ()=>{
     sliding.style.height = '0px';
 })
+
+contact.addEventListener('mouseenter', ()=>{
+    contactlist.style.height = contactlist.scrollHeight + 'px';
+})
+
+contact.addEventListener('mouseleave', ()=>{
+    contactlist.style.height = '0px';
+});
